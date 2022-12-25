14b3bc5354942728f2ee51f4d6bb8316af658232
Luhmeiy <Medeiros.luizhenrique@yahoo.com.br>
2022-12-24 21:33:49 -0300

    Update

--- My First Node.js Server/My First Node.js Server (MVC)/controllers/products.js
+++ My First Node.js Server/My First Node.js Server (MVC)/controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res) => {
     res.render('add-product', { 
@@ -8,14 +8,17 @@ exports.getAddProduct = (req, res) => {
 }
 
 exports.postAddProduct = (req, res) => {
-    products.push({ title: req.body.title });
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 }
 
 exports.getProducts = (req, res) => {
-    res.render('shop', { 
-        prods: products, 
-        pageTitle: 'Shop', 
-        path: '/'
+    Product.fetchAll((products) => {
+        res.render('shop', { 
+            prods: products, 
+            pageTitle: 'Shop', 
+            path: '/'
+        });
     });
 }
