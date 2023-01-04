bed18ebe7d02149966f3b4e7cc7f08db46365892
Luhmeiy <Medeiros.luizhenrique@yahoo.com.br>
2023-01-04 13:56:06 -0300

    Update MongoDB version

--- My First Node.js Server/My First Node.js Server (MVC)/(MongoDB)/util/database.js
+++ My First Node.js Server/My First Node.js Server (MVC)/(MongoDB)/util/database.js
@@ -4,7 +4,7 @@ const MongoClient = mongodb.MongoClient;
 let _db;
 
 const mongoConnect = callback => {
-    MongoClient.connect('mongodb+srv://Medeiros:Ayumi1207@node-complete-cluster.bwsosjp.mongodb.net/shop?retryWrites=true&w=majority')
+    MongoClient.connect('mongodb+srv://Medeiros:nodeComplete@node-complete-cluster.bwsosjp.mongodb.net/shop?retryWrites=true&w=majority')
         .then(client => {
             console.log('Connected!');
             _db = client.db();
